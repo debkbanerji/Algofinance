5bbe673b9c858ff54cade5fa6b4a4ae4083ec4fe
cking90 <cking90@gatech.edu>
2019-03-30 14:55:56 -0400

    Add calculate policy component

---
+++ src/app/calculate-policy/calculate-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate-policy',
+  templateUrl: './calculate-policy.component.html',
+  styleUrls: ['./calculate-policy.component.css']
+})
+export class CalculatePolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
