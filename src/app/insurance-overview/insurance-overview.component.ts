b43a3fbb68a0af8fca19ed01cbd089718699f9af
cking90 <cking90@gatech.edu>
2019-03-30 14:05:37 -0400

    Stub out input insurance item

--- src/app/insurance-overview/insurance-overview.component.ts
+++ src/app/insurance-overview/insurance-overview.component.ts
@@ -1,5 +1,5 @@
 import {Component, NgZone, OnInit} from '@angular/core';
-import {AngularFireDatabase, } from '@angular/fire/database';
+import {AngularFireDatabase, AngularFireList,} from '@angular/fire/database';
 import {AuthService} from '../providers/auth.service';
 import {Router} from '@angular/router';
 import {Observable} from 'rxjs';
@@ -11,7 +11,13 @@ import {Observable} from 'rxjs';
 })
 export class InsuranceOverviewComponent implements OnInit {
 
-    public insuranceItems: Observable<any[]>;
+    public insuranceItemsValueChanges: Observable<any[]>;
+    private insuranceItemsList: AngularFireList<any>;
+
+    public newName: string;
+    public newCost: number;
+    public newDescription: string;
+    public newImportance: number;
 
     constructor(private db: AngularFireDatabase,
                 public authService: AuthService,
@@ -23,7 +29,8 @@ export class InsuranceOverviewComponent implements OnInit {
             if (auth != null) {
                 const path = '/user-insurance-lists/' + auth.uid;
                 component.ngZone.run(function () {
-                    component.insuranceItems = db.list(path).valueChanges();
+                    component.insuranceItemsList = db.list(path);
+                    component.insuranceItemsValueChanges = component.insuranceItemsList.valueChanges();
                 });
             } else {
                 component.ngZone.run(function () {
@@ -34,6 +41,25 @@ export class InsuranceOverviewComponent implements OnInit {
     }
 
     ngOnInit() {
+    }
+
+    onAddInsuranceItemSubmit() {
+        // const component = this;
+        // if (component.newCost)
+        // let shouldAdd = false;
+        //
+        // if (shouldAdd) {
+
+        //}
+    }
+
+    addInsuranceItem(name, cost, description, importance) {
+        this.insuranceItemsList.push({
+            'name': name,
+            'cost': cost,
+            'description': description,
+            'importance': importance
+        });
     }
 
 }
