7aac55749662c1604c53e4eae935f7fb562a5507
cking90 <cking90@gatech.edu>
2019-03-30 20:19:18 -0400

    Stub front-end and backend for viewing user profiles

--- src/app/view-clients/view-clients.component.ts
+++ src/app/view-clients/view-clients.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {AngularFireDatabase, AngularFireList,} from '@angular/fire/database';
+import {AuthService} from '../providers/auth.service';
+import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
 
 @Component({
-  selector: 'app-view-clients',
-  templateUrl: './view-clients.component.html',
-  styleUrls: ['./view-clients.component.css']
+    selector: 'app-insurance-overview',
+    templateUrl: './insurance-overview.component.html',
+    styleUrls: ['./insurance-overview.component.css'],
 })
 export class ViewClientsComponent implements OnInit {
 
-  constructor() { }
+    public userValueChanges: Observable<any[]>;
 
-  ngOnInit() {
-  }
+    private userList: AngularFireList<any>;
+
+    public userUID: string;
+
+    constructor(private db: AngularFireDatabase,
+                public authService: AuthService,
+                private router: Router,
+                private ngZone: NgZone
+    ) {
+        const component = this;
+        component.authService.afAuth.auth.onAuthStateChanged((auth) => {
+            if (auth != null) {
+                const path = '/user-profiles';
+                //component.userUID = auth.uid;
+                component.ngZone.run(function () {
+                    component.userList = db.list(path);
+                    component.userValueChanges = component.userList.snapshotChanges();
+
+                });
+            } else {
+                component.ngZone.run(function () {
+                    component.router.navigate(['login']);
+                });
+            }
+        });
+    }
+
+    ngOnInit() {
+    }
+
+
+
 
 }
