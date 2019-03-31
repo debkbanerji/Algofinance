ce070c9e2631d733f8f1e49028254b1f22dee998
deb <debkbanerji@gmail.com>
2019-03-31 01:54:32 -0400

    Add agent sign in hashes

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit, OnDestroy, NgZone} from '@angular/core';
 import {Router} from '@angular/router';
-
 import {AngularFireDatabase} from '@angular/fire/database';
-
 import {AuthService} from '../providers/auth.service';
 import {Subscription} from 'rxjs';
+
+declare let sjcl;
 
 @Component({
     selector: 'app-login-page',
@@ -15,8 +15,15 @@ export class LoginPageComponent implements OnInit, OnDestroy {
     private userDataSubscription: Subscription;
     public LOGO_URL: string;
     public GOOGLE_URL: string;
-    public SMART_URL: string;
     public loginAsAgent: boolean;
+    public agentPassKey: string = '';
+
+    public validHashes = [
+        '88D4266FD4E6338D13B845FCF289579D209C897823B9217DA3E161936F031589',
+        '297ECEBFA892D9186F261AAE5953B6B61C45A2587FFF26145F9CAC74DF060FA2',
+        'F4AA0655CDB8D4FCF6F719C7A786DE10556783C70BFB8EF1D78923482FE6EBBC',
+        'B9DD960C1753459A78115D3CB845A57D924B6877E805B08BD01086CCDF34433C'
+    ];
 
     constructor(public authService: AuthService, private db: AngularFireDatabase, private router: Router, private ngZone: NgZone) {
     }
@@ -30,6 +37,15 @@ export class LoginPageComponent implements OnInit, OnDestroy {
         const component = this;
         this.authService.loginWithGoogle().then((loginData) => {
             this.authService.afAuth.auth.onAuthStateChanged((auth) => {
+                const bitArray = sjcl.hash.sha256.hash(component.agentPassKey.toLowerCase());
+                const passKeyHash = sjcl.codec.hex.fromBits(bitArray);
+                let containsHash = false;
+                for (let i = 0; i < component.validHashes.length; i++) {
+                    containsHash = containsHash || (component.validHashes[i].toLowerCase() === passKeyHash);
+                }
+                if (!containsHash) {
+                    component.loginAsAgent = false;
+                }
                 if (auth != null) {
                     const profileObject = component.db.object('/' + (component.loginAsAgent ? 'agent' : 'user') + '-profiles/' + auth.uid);
                     profileObject.set({
